920b0cd569c217d51024b6486f0db5160334b803
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2022-07-13 10:48:44 +0300

    Add OS specific settings

--- brew/install_essential_brews_and_casks.sh
+++ brew/install_essential_brews_and_casks.sh
@@ -47,3 +47,29 @@ yes | brew upgrade
 
 # Remove outdated versions from the cellar
 brew cleanup
+
+echo "Configuring Mac OS settings...";
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Set a faster key repeat
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo "All done here!"
