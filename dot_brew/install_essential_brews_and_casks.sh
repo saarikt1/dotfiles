33603cbcd01786ff33968c84c6e4b5324eab5d73
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2022-07-13 13:14:12 +0300

    Modify Homebrew installation script

--- dot_brew/install_essential_brews_and_casks.sh
+++ dot_brew/install_essential_brews_and_casks.sh
@@ -14,7 +14,7 @@ echo "Checking if Homebrew is already installed...";
 # Credit: https://gist.github.com/codeinthehole/26b37efa67041e1307db
 if test ! $(which brew); then
   echo "Installing Homebrew...";
-  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew is already installed...";
 fi
